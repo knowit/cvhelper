f3ad6e4e2949cd45a0a192a91a9158025fdb36ff
A-Stangeland <aleksander.stangeland@gmail.com>
2023-12-19 08:37:53 +0100

    Add settings file

---
+++ settings.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+
+class Settings(BaseSettings):
+    ollama_url: str = "http://localhost:11434"
+    ollama_model: str = "mistral"
+    chroma_host: str = "localhost"
+    chroma_port: str = "8080"
+    cvpartner_token: str = Field(
+        alias="CVPARTNER_TOKEN", description="Token for the CV Partner API."
+    )
+
+
+settings = Settings()
